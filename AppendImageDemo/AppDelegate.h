209a63d0725d1a70052142103479df15f42fff3d
yangshucheng <yangshucheng@yangshuchengdeimac.local>
2017-06-13 07:52:26 +0800

    Initial Commit

---
+++ AppendImageDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppendImageDemo
+//
+//  Created by yangshucheng on 2017/6/13.
+//  Copyright © 2017年 yangshucheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppendImageDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppendImageDemo
+//
+//  Created by yangshucheng on 2017/6/13.
+//  Copyright © 2017年 yangshucheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
